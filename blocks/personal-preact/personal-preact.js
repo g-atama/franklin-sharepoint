a20b6b248659b8266ff406f6ceb4c71e59f54890
Kevin Mitchell <>
2023-11-28 20:35:27 +0900

    load data outside of main event loop, without await

---
+++ blocks/personal-preact/personal-preact.js
@@ -0,0 +1,67 @@
+/* eslint-disable no-console */
+/* eslint-disable class-methods-use-this */
+// Create a class for the element
+class ArcPersonalizedEdge extends HTMLElement {
+  connectedCallback() {
+    // Create a shadow root
+    const shadow = this.attachShadow({ mode: 'open' });
+
+    // Create spans
+    const wrapper = document.createElement('span');
+    wrapper.setAttribute('class', 'wrapper');
+
+    const priceInfo = document.createElement('p');
+    priceInfo.setAttribute('class', 'price');
+    const price = this.getAttribute('data-price');
+    const currency = this.getAttribute('data-currency');
+    priceInfo.textContent = `Price is ${currency}${price}`;
+
+    const availabilityInfo = document.createElement('p');
+    availabilityInfo.setAttribute('class', 'availability');
+    const availability = this.getAttribute('data-available');
+    availabilityInfo.textContent = `The product is ${availability === 'true' ? '' : 'not '}available`;
+
+    // Create some CSS to apply to the shadow dom
+    const style = document.createElement('style');
+
+    style.textContent = `
+     
+    `;
+
+    // Attach the created elements to the shadow dom
+    shadow.appendChild(style);
+    shadow.appendChild(wrapper);
+    wrapper.appendChild(priceInfo);
+    wrapper.appendChild(availabilityInfo);
+  }
+
+  attributeChangedCallback(name, oldValue, newValue) {
+    console.log(
+      `Attribute ${name} has changed from ${oldValue} to ${newValue}.`,
+    );
+  }
+}
+
+customElements.define('arc-volatile-data', ArcPersonalizedEdge);
+
+export default async function decorate(block) {
+  // eslint-disable-next-line no-undef
+  const slug = window.location.pathname.split('/').at(-1);
+  try {
+    const result = await fetch(`/_data/commerce/volatile/${slug}`);
+    const volatileData = await result.json();
+
+    console.log('volatileData', volatileData);
+
+    const volatileDataDisplay = document.createElement('arc-volatile-data');
+    // work
+    volatileDataDisplay.setAttribute('data-price', volatileData.pricing.price);
+    volatileDataDisplay.setAttribute('data-currency', volatileData.pricing.currency);
+    volatileDataDisplay.setAttribute('data-available', volatileData.available);
+    block.textContent = '';
+    block.append(volatileDataDisplay);
+  } catch (error) {
+    // no op
+    // console.log('could not fetch data', error);
+  }
+}
--- blocks/volatile/volatile.js
+++ blocks/volatile/volatile.js
@@ -1,67 +1,135 @@
 /* eslint-disable no-console */
 /* eslint-disable class-methods-use-this */
+
+const template = document.createElement('template');
+template.innerHTML = `
+<div id="volatile">
+  <div id="pricing"><span id="price"></span><span id="currency"></span></div>
+  <div id="availability" ></div>
+</div>
+`;
+
 // Create a class for the element
 class ArcVolatileCommerceData extends HTMLElement {
+  price = '';
+
+  currency = '';
+
+  available = false;
+
+  constructor() {
+    super();
+    this.attachShadow({ mode: 'open' });
+    this.shadowRoot.appendChild(template.content.cloneNode(true));
+  }
+
+  static get observedAttributes() {
+    return ['price', 'currency', 'available'];
+  }
+
+  get priceElement() {
+    return this.shadowRoot.getElementById('price');
+  }
+
+  get volatileElement() {
+    return this.shadowRoot.getElementById('volatile');
+  }
+
+  get currencyElement() {
+    return this.shadowRoot.getElementById('currency');
+  }
+
+  get availabilityElement() {
+    return this.shadowRoot.getElementById('availability');
+  }
+
+  setValues(show) {
+    if (show) {
+      this.volatileElement.classList.add('show');
+    }
+    this.priceElement.innerHTML = this.price;
+    this.availabilityElement.innerHTML = this.available ? 'Available' : 'Not available';
+    this.currencyElement.innerHTML = this.currency;
+  }
+
   connectedCallback() {
-    // Create a shadow root
-    const shadow = this.attachShadow({ mode: 'open' });
-
-    // Create spans
-    const wrapper = document.createElement('span');
-    wrapper.setAttribute('class', 'wrapper');
-
-    const priceInfo = document.createElement('p');
-    priceInfo.setAttribute('class', 'price');
-    const price = this.getAttribute('data-price');
-    const currency = this.getAttribute('data-currency');
-    priceInfo.textContent = `Price is ${currency}${price}`;
-
-    const availabilityInfo = document.createElement('p');
-    availabilityInfo.setAttribute('class', 'availability');
-    const availability = this.getAttribute('data-available');
-    availabilityInfo.textContent = `The product is ${availability === 'true' ? '' : 'not '}available`;
+    this.price = this.getAttribute('price') || '...';
+    this.currency = this.getAttribute('currency') || '...';
+    this.available = this.getAttribute('available') || false;
 
     // Create some CSS to apply to the shadow dom
     const style = document.createElement('style');
 
     style.textContent = `
-     
+      #volatile {
+        opacity: 0;
+        transition: opacity 175ms ease-in, visibility 0ms ease-in 250ms;
+      }
+
+      #volatile.show {
+        visibility: visible;
+        opacity: 1;
+      }
     `;
 
-    // Attach the created elements to the shadow dom
-    shadow.appendChild(style);
-    shadow.appendChild(wrapper);
-    wrapper.appendChild(priceInfo);
-    wrapper.appendChild(availabilityInfo);
+    this.shadowRoot.appendChild(style);
+
+    this.setValues();
   }
 
   attributeChangedCallback(name, oldValue, newValue) {
-    console.log(
-      `Attribute ${name} has changed from ${oldValue} to ${newValue}.`,
-    );
+    switch (name) {
+      case 'available':
+        this.available = newValue;
+        this.setValues(true);
+        break;
+
+      case 'price':
+        this.price = newValue;
+        this.setValues(true);
+        break;
+
+      case 'currency':
+        this.currency = newValue;
+        this.setValues(true);
+        break;
+
+      default:
+        break;
+    }
   }
 }
 
 customElements.define('arc-volatile-data', ArcVolatileCommerceData);
 
-export default async function decorate(block) {
+async function loadPrice(volatileDataDisplay) {
   // eslint-disable-next-line no-undef
   const slug = window.location.pathname.split('/').at(-1);
-  try {
-    const result = await fetch(`/_data/commerce/volatile/${slug}`);
-    const volatileData = await result.json();
+  const result = await fetch(`/_data/commerce/volatile/${slug}`);
+  const volatileData = await result.json();
 
-    console.log('volatileData', volatileData);
+  // await new Promise((resolve) => setTimeout(resolve, 3000)).then(() => console.log('OK'));
 
-    const volatileDataDisplay = document.createElement('arc-volatile-data');
-    // work
-    volatileDataDisplay.setAttribute('data-price', volatileData.pricing.price);
-    volatileDataDisplay.setAttribute('data-currency', volatileData.pricing.currency);
-    volatileDataDisplay.setAttribute('data-available', volatileData.available);
-    block.textContent = '';
-    block.append(volatileDataDisplay);
-  } catch (error) {
-    // no op
-    // console.log('could not fetch data', error);
-  }
+  // const volatileData = {
+  //   pricing: {
+  //     price: '123',
+  //     currency: 'asdf',
+
+  //   },
+  //   available: true,
+  // };
+
+  // console.log('volatileData', volatileData);
+
+  // work
+  volatileDataDisplay.setAttribute('price', volatileData.pricing.price);
+  volatileDataDisplay.setAttribute('currency', volatileData.pricing.currency);
+  volatileDataDisplay.setAttribute('available', volatileData.available);
+}
+
+export default async function decorate(block) {
+  const volatileDataDisplay = document.createElement('arc-volatile-data');
+  block.textContent = '';
+  block.append(volatileDataDisplay);
+  loadPrice(volatileDataDisplay);
 }
